9678392a80b571b87482f5cc021e16f799f8423f
Kalmar41k <vitalik01082003@gmail.com>
2024-02-08 17:04:58 +0200

    Edited SQL Script :Columns product_sort and product_type are unique and column grade has type as integer now

--- SQLScripts/000 FruitFolio-database.sql
+++ SQLScripts/000 FruitFolio-database.sql
@@ -9,7 +9,7 @@ CREATE TABLE Product_sorts (
 	ID SERIAL PRIMARY KEY,
 	Product_class varchar not NULL,
 	Product_type_id integer NOT NULL,
-	Product_sort varchar NOT NULL,
+	Product_sort varchar NOT NULL UNIQUE,
 	Num_of_comments integer NOT NULL,
 	Num_of_grades integer NOT NULL,
 	Mean_grade double precision NOT NULL
@@ -17,14 +17,14 @@ CREATE TABLE Product_sorts (
 
 CREATE TABLE Product_types (
 	ID SERIAL PRIMARY KEY,
-	Product_type varchar NOT NULL
+	Product_type varchar NOT NULL UNIQUE
 );
 
 CREATE TABLE Product_grades (
 	ID SERIAL PRIMARY KEY,
 	User_id integer NOT NULL,
 	Product_sort_id integer NOT NULL,
-	Grade bytea NOT NULL
+	Grade integer NOT NULL
 );
 
 CREATE TABLE Product_comments (
@@ -34,8 +34,6 @@ CREATE TABLE Product_comments (
 	Comment text NOT NULL,
 	Date_time TIMESTAMP NOT NULL
 );
-
-DROP TABLE Product_comments
 
 ALTER TABLE Product_sorts ADD CONSTRAINT Sorts_Types FOREIGN KEY (Product_type_id) REFERENCES Product_types (ID);
 
@@ -47,4 +45,4 @@ ALTER TABLE Product_comments ADD CONSTRAINT Comments_Users FOREIGN KEY (User_id)
 
 ALTER TABLE Product_comments ADD CONSTRAINT Comments_Sorts FOREIGN KEY (Product_sort) REFERENCES Product_sorts (ID);
 
-
+ALTER TABLE product_types ALTER COLUMN product_type TYPE integer UNIQUE
