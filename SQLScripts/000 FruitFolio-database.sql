e08ddad66832d821e2ef6cdba0807a0e847b9207
Kalmar41k <vitalik01082003@gmail.com>
2024-02-08 15:57:47 +0200

    Updated SQL Script (column status in table product_sorts has renamed as product_class now

--- SQLScripts/000 FruitFolio-database.sql
+++ SQLScripts/000 FruitFolio-database.sql
@@ -5,11 +5,9 @@ CREATE TABLE Users (
 	salt varchar NOT NULL
 );
 
-CREATE TYPE Product_class as enum ('Fruit', 'Vegetable', 'Berry');
-
 CREATE TABLE Product_sorts (
 	ID SERIAL PRIMARY KEY,
-	status Product_class,
+	Product_class varchar not NULL,
 	Product_type_id integer NOT NULL,
 	Product_sort varchar NOT NULL,
 	Num_of_comments integer NOT NULL,
@@ -37,6 +35,8 @@ CREATE TABLE Product_comments (
 	Date_time TIMESTAMP NOT NULL
 );
 
+DROP TABLE Product_comments
+
 ALTER TABLE Product_sorts ADD CONSTRAINT Sorts_Types FOREIGN KEY (Product_type_id) REFERENCES Product_types (ID);
 
 ALTER TABLE Product_grades ADD CONSTRAINT Grades_Users FOREIGN KEY (User_id) REFERENCES Users (ID);
@@ -46,8 +46,5 @@ ALTER TABLE Product_grades ADD CONSTRAINT Grades_Sorts FOREIGN KEY (Product_sort
 ALTER TABLE Product_comments ADD CONSTRAINT Comments_Users FOREIGN KEY (User_id) REFERENCES Users (ID);
 
 ALTER TABLE Product_comments ADD CONSTRAINT Comments_Sorts FOREIGN KEY (Product_sort) REFERENCES Product_sorts (ID);
-
-
-
 
 
